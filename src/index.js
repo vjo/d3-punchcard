ff0632a02e35452be5e906654c897f350f564cd0
Victor Jolissaint <victor.jolissaint@gmail.com>
2017-02-10 00:47:32 +0000

    add config and init function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-export default function () {
-  return 42;
+import d3 from 'd3';
+
+const defaults = {
+  target: 'svg',
+  width: 800,
+  height: 400, // (400 - 15) / 7?
+  margin: {
+    top: 15,
+    right: 0,
+    bottom: 15,
+    left: 100,
+  },
+  timeFormat: '%H',
+  tickSize: 12,
+  maxDotRadius: 14,
+  data: undefined,
+};
+
+class Punchcard {
+  constructor(config) {
+    this.set(config);
+    this.init();
+  }
+
+  set(config) {
+    Object.assign(this, defaults, config);
+  }
+
+  init() {
+    this.element = d3.select(this.target);
+    this.offset = this.element.node().getBoundingClientRect();
+    this.formatTime = d3.timeFormat(this.timeFormat);
+  }
 }
+
+module.exports = Punchcard;
