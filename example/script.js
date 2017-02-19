9949faacef879e15fa163ba685d88b7c99073667
Victor Jolissaint <victor.jolissaint@gmail.com>
2017-02-19 23:20:31 +0000

    Add new example with custom style

--- example/script.js
+++ example/script.js
@@ -21,7 +21,26 @@ var data = [{
   values: [2,0,0,0,0,0,0,1,0,0,2,2,2,2,1,1,3,0,0,2,2,1,0,3],
 }];
 
-var punchcard0 = new punchcard({
-  target: '#chart',
+var chart0 = new punchcard({
+  target: '#chart0',
   data: data,
+});
+
+var chart1 = new punchcard({
+  target: '#chart1',
+  data: data,
+  width: 600,
+  height: 400,
+  maxDotRadius: 10,
+  tickSize: 10,
+  styles: {
+    axisLine: { stroke: '#93a5cf', 'stroke-width': '2px' },
+    rowTick: { stroke: '#93a5cf', 'stroke-width': '2px' },
+    dot: { fill: '#fe6e86' },
+    label: {
+      fill: '#71be74',
+      'text-anchor': 'start',
+    },
+    xAxisText: { stroke: '#93a5cf' },
+  },
 });
